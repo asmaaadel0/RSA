f09597babbfc1cc4d3a4648b980391f42ef9f20e
Asmaa Adel <88618793+asmaaadel0@users.noreply.github.com>
2023-03-18 20:41:00 +0200

    create some sender functions

--- sender.py
+++ sender.py
@@ -1,5 +1,40 @@
 import socket
+import numpy as np
+def splitToGroups(message):
+    splited_message = []
+    for i in range(0,len(message),5):
+      splited_message.append(message[i:i+5].lower())
+    if(len(splited_message[-1]) != 5):
+        end = splited_message[-1]
+        for i in range(len(splited_message[-1]), 5):
+            end = end + ' '
+        splited_message[-1] = end    
+    splited_message = np.asarray(splited_message)
+    return splited_message  
 
+def convertToInt(splited_message):
+    numbers = []
+    for group in splited_message:
+        plaintext_number = 0
+        number = 0
+        i = 4
+        for char in group:
+            if(ord(char) in range(48, 57)):
+              number = ord(char) - 48
+            elif(ord(char) in range(97, 122)):
+              number = ord(char) - 87
+            else:
+              number = 36
+            plaintext_number = plaintext_number + 37**i * number
+            i = i - 1
+        numbers.append(plaintext_number)
+    return numbers    
+    
+
+def encryption(message):
+    splited_message = splitToGroups(message)
+    converted_message = convertToInt(splited_message)
+    return message
 
 def sender_program():
     # get the hostname
@@ -23,6 +58,7 @@ def sender_program():
             break
         # print("from connected reciever: " + str(data))
         # data = input(' -> ')
+        data = encryption(data)
         conn.send(data.encode())  # send data to the reciever
 
     conn.close()  # close the connection
